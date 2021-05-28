2af2c3503606627dff79b62ae4945b089d4bd862
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-28 17:22:42 +0300

    feat: grid

--- src/components/Apartments/Apartments.tsx
+++ src/components/Apartments/Apartments.tsx
@@ -9,6 +9,27 @@ const Apartments = () => (
     <ul className={styles.apartments}>
       <li className={styles.apartments__item}>
         <img className={styles.apartments__image} alt="" src={image1} />
+        <div className={`${styles.apartments__star} ${styles.apartments__star_empty}`} />
+        <h2 className={styles.apartments__title}>3-х комнатная на Минской</h2>
+        <div className={styles.apartments__description}>
+          <ul>
+            <li className={styles.apartments__info}>
+              <strong>Адрес:</strong> Тюмень, Минская ул, 3а, кв 123
+            </li>
+            <li className={styles.apartments__info}>
+              <strong>Комнат:</strong> 3
+            </li>
+            <li className={styles.apartments__info}>
+              <strong>Площадь:</strong> 123 м&sup2;
+            </li>
+            <li className={styles.apartments__agent}>
+              <strong>Агент:</strong> Дроздов Василий Михайлович
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li className={styles.apartments__item}>
+        <img className={styles.apartments__image} alt="" src={image1} />
         <h2 className={styles.apartments__title}>3-х комнатная на Минской</h2>
         <div className={styles.apartments__description}>
           <ul>
