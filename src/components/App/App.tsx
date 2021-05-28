033a0091129acbc8f0e16bbf59ae02c70278079f
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-28 13:03:59 +0300

    feat: header

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Header from 'components/Header/Header';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss';
+
+const Header = () => (
+  <header className={styles.header}>
+    <h1 className={styles.header__title}>Smooth Apartments</h1>
+  </header>
+);
+
+export default Header;
