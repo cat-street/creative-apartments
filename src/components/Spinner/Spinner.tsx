70b2efca5ec3f8cd53565af39d52871fc4333682
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-29 10:09:53 +0300

    fix: scss

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import styles from './Spinner.module.css';
+import styles from './Spinner.module.scss';
 
 const Spinner: FC = () => (
   <div className={styles.loader}>Loading...</div>
